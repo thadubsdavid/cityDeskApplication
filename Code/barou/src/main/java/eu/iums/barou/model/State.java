a9379175296b36013ff6a3fd4f4deb96432afda3
Annika <roederanni@gmx.de>
2018-06-14 14:38:19 +0200

    MenüButton

---
+++ Code/barou/src/main/java/eu/iums/barou/model/State.java
@@ -0,0 +1,42 @@
+package eu.iums.barou.model;
+
+import com.sothawo.mapjfx.Coordinate;
+import eu.iums.barou.controllers.POI;
+import eu.iums.barou.controllers.Route;
+
+public class State {
+
+    private  static State instance;
+    private Coordinate startpunkt;
+//    private Ort ort;
+    private POI poi;
+    private Route route;
+
+
+    private State(){
+
+    }
+
+    public static State getInstance(){
+        // immer nur 1 Objekt initialisiert
+        if(instance == null){
+            instance = new State();
+        }
+        return instance;
+    }
+
+    public Coordinate getStartpunkt() {
+        return startpunkt;
+    }
+
+    public void setStartpunkt(Coordinate startpunkt) {
+        this.startpunkt = startpunkt;
+    }
+
+ //   public Ort getOrt () {return ort;}
+ //   public void setOrt (Ort ort) {this.ort = ort;}
+
+    public POI getPoi () {return poi;}
+    public void setPoi (POI poi) {this.poi = poi;}
+
+}
