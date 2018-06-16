f9a3677198dcd1ba97db73901756ca8cb0e20a9a
Annika <roederanni@gmx.de>
2018-06-16 11:30:56 +0200

    neue Trias und Wheelmap API hinzugefügt State Klasse ergänzt

--- Code/barou/src/main/java/eu/iums/barou/model/State.java
+++ Code/barou/src/main/java/eu/iums/barou/model/State.java
@@ -2,6 +2,7 @@ package eu.iums.barou.model;
 
 import com.sothawo.mapjfx.Coordinate;
 import eu.iums.barou.controllers.Route;
+import eu.iums.trias.api.Trip;
 
 import javax.xml.soap.Node;
 import java.util.List;
@@ -21,6 +22,37 @@ public class State {
     private  static State instance;
     private Coordinate startpunkt;
     private Route route;
+
+    public List<Trip> getTripList() {
+        return tripList;
+    }
+
+    public void setTripList(List<Trip> tripList) {
+        this.tripList = tripList;
+    }
+
+    private List<Trip> tripList;
+
+    public static void setInstance(State instance) {
+        State.instance = instance;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public List<Node> getPoiListe() {
+        return poiListe;
+    }
+
+    public void setPoiListe(List<Node> poiListe) {
+        this.poiListe = poiListe;
+    }
+
     private List<Node> poiListe;
 
 
